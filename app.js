0f28d7f274e675626b1c27c74822b155f62581c5
PrivacyPolicy <GabeHWebsites@gmail.com>
2017-07-14 21:29:47 +0000

    Now actually sends statuses back (hopefully)

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// To listen for pull requests
 var githubhook = require('githubhook');
 var github = githubhook({
   host: '0.0.0.0',
@@ -6,10 +7,29 @@ var github = githubhook({
   secret: process.env['TEST_GITHUB_CI_SERVER_SECRET']
 });
 
+// To update commit statuses
+var octonode = require('octonode');
+var gitHubAccess = process.env['ONAROLL_GITHUB_ACCESS_TOKEN'];
+var client = octonode.client(gitHubAccess);
+
+
+// Listen for pull requests and return the status
 github.listen();
 
-github.on('push', function(repo, ref, data) {
-  console.log('\n\nrepo: ' + repo);
-  console.log('\n\nref: ' + ref);
-  console.log('\n\ndata: ' + data);
+github.on('pull_request', function(pull_request) {
+  //repo, ref, data) {
+  // console.log('\n\nrepo: ' + repo);
+  // console.log('\n\nref: ' + ref);
+  // console.log('\n\ndata: ' + data);
+  client.create_status(
+    pull_request['base']['repo']['full_name'],
+    pull_request['head']['sha'],
+    'pending');
+  setTimeout(function() {
+    client.create_status(
+      pull_request['base']['repo']['full_name'],
+      pull_request['head']['sha'],
+      'success');
+  }, 10000);
+  console.log('Lol tricked them into thinking we did something but we did nothing');
 });
